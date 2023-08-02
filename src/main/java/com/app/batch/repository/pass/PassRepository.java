e5502d3df6e8fae55dbc256f923a0b9aa9715bd5
LimJiNa <jn4624@gmail.com>
2023-08-02 13:07:11 +0900

    수업 종료 후 이용권 차감 complete - close #9

--- src/main/java/com/app/batch/repository/pass/PassRepository.java
+++ src/main/java/com/app/batch/repository/pass/PassRepository.java
@@ -1,8 +1,18 @@
 package com.app.batch.repository.pass;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface PassRepository extends JpaRepository<PassEntity, Integer> {
+    @Transactional
+    @Modifying
+    @Query(value =  "UPDATE PassEntity p" +
+                    "SET p.remainingCount = :remainingcount," +
+                        "p.moditiedAt = CURRENT_TIMESTAMP" +
+                    "WHERE p.passSeq = :passSeq")
+    int updateRemainingCount(Integer passSeq, Integer remainingCount);
 }
